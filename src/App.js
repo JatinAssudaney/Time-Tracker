2c0bcc270b6889141d9b39b088f01ad037de4732
JatinAssudaney <jatin.assudaney24@gmail.com>
2021-01-10 11:43:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
